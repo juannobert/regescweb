1e4ae7f21b9c58623d4c5282e72dd41d2f98480e
Juan Nobert <nobert407@gmail.com>
2022-06-06 14:19:27 -0300

    Initial commit

---
+++ src/test/java/br/com/juannobert/regescweb/RegescwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juannobert.regescweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegescwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
