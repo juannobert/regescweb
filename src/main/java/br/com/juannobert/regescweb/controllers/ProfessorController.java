8e7109c578aa5efa1fcbb354c6c8fe4f286a1918
Juan Nobert <nobert407@gmail.com>
2022-06-07 21:46:28 -0300

    including database data in table

--- src/main/java/br/com/juannobert/regescweb/controllers/ProfessorController.java
+++ src/main/java/br/com/juannobert/regescweb/controllers/ProfessorController.java
@@ -1,16 +1,23 @@
 package br.com.juannobert.regescweb.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.juannobert.regescweb.services.ProfessorServices;
 
 @Controller
 @RequestMapping("/professores")
 public class ProfessorController {
-
+	@Autowired
+	ProfessorServices professorServices;
 	@GetMapping()
-	public String findAll() {
-		return "professores/index";
+	public ModelAndView findAll() {
+		ModelAndView mv = new ModelAndView("professores/index");
+		mv.addObject("professores", professorServices.findAll());
+		return mv;
 		
 	}
 	
